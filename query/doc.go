f31f8ed3b79da61d72bc638724f9a6f6d5ae025d
goliatone <140582+goliatone@users.noreply.github.com>
2025-12-18 18:41:43 -0800

    chore: add query components

---
+++ query/doc.go
@@ -0,0 +1,2 @@
+// Package query hosts go-command query handlers for go-export.
+package query
---
+++ query/placeholder_test.go
@@ -0,0 +1,7 @@
+package query
+
+import "testing"
+
+func TestPlaceholder(t *testing.T) {
+	t.Skip("placeholder for query adapter tests")
+}
