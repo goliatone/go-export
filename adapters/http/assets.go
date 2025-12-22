dc89977e7331863e1502a2af8f8a7eee91e5c333
goliatone <140582+goliatone@users.noreply.github.com>
2025-12-22 00:14:07 -0800

    add: pdf asset handler

--- adapters/http/assets.go
+++ adapters/http/assets.go
@@ -10,3 +10,22 @@ import (
 func RuntimeAssetsHandler() http.Handler {
 	return http.FileServer(http.FS(export.RuntimeAssetsFS()))
 }
+
+// PDFAssetsHandler serves embedded PDF preview assets (echarts, jsPDF, html2canvas).
+func PDFAssetsHandler(prefix string) http.Handler {
+	if prefix == "" {
+		prefix = export.DefaultPDFAssetsPath
+	}
+	prefix = ensureTrailingSlash(prefix)
+	return http.StripPrefix(prefix, http.FileServer(http.FS(export.PDFAssetsFS())))
+}
+
+func ensureTrailingSlash(value string) string {
+	if value == "" {
+		return ""
+	}
+	if value[len(value)-1] == '/' {
+		return value
+	}
+	return value + "/"
+}
